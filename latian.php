fea4b1d82d5cbb8654e7e2fe782df2ecd6d8dabf
Najmi Khaeri Arrisa Putra <najmikhaeriarrisaputra@gmail.com>
2022-06-21 20:33:15 +0700

    first commit

---
+++ latian.php
@@ -0,0 +1,31 @@
+<?php
+echo "OPERATOR ARITHMATICS";
+echo "<br>";
+echo "<br>";
+$x = 10;  
+$y = 3;
+echo $x + $y;
+echo"<br>";
+echo $x - $y;
+echo"<br>";
+echo $x * $y;
+echo"<br>";
+echo $x / $y;
+echo"<br>";
+echo $x % $y;
+echo"<br>";
+echo $x ** $y;
+echo"<br>";
+echo $x * $y +2;
+echo "<br>";
+echo "<br>";
+echo "PHP COMPARISON";
+echo "<br>";
+echo "<br>";
+var_dump($x == $y); // returns true because values are equal
+echo "<br>";
+var_dump($x === $y); // returns true because values are equal
+echo "<br>";
+var_dump($x != $y); // returns true because values are equal
+echo ""
+?>
---
+++ soal.php
@@ -0,0 +1,9 @@
+<?php
+
+$nasabah = 15000000;
+$bunga = 2/100;
+$akhir = 10;
+echo $nasabah * $bunga * $akhir + $nasabah;
+
+
+?>
